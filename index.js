593f36e22ab34bfad7e32a1c4ca22580a8319f20
rubayet-islam <season.ss2001@gmail.com>
2022-11-08 13:51:18 +0600

    mongodb connected and services API created

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+//HarryDB
+//YmIgPJXDSvB42AYC
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsmlaiu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("HarryDB").collection("services");
+
+    app.get('/services', async (req, res) => {
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    });
+
+
+
+  } finally {
+  }
+}
+
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Harry is running");
+});
+
+app.listen(port, () => {
+  console.log(`Harry server running on ${port}`);
+});
