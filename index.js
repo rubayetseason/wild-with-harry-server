82cc6c06826e63c9cf87d72cdd9b125358420f5c
rubayet-islam <season.ss2001@gmail.com>
2022-11-08 15:13:33 +0600

    dynamic service route added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -31,14 +31,19 @@ async function run() {
       res.send(services);
     });
 
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
     app.get("/limited", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const limitedService = await cursor.limit(3).toArray();
       res.send(limitedService);
     });
-
-    
   } finally {
   }
 }
