4981eba4b75f4a76946e2559306770d8ef8817a9
rubayet-islam <season.ss2001@gmail.com>
2022-11-10 04:26:41 +0600

    review section api completed

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//HarryDB
-//YmIgPJXDSvB42AYC
+//wildDB
+//4t6d8W7wCskoQaqg
+//mongodb://localhost:27017
+// const uri = 'mongodb://localhost:27017';
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsmlaiu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -45,10 +47,33 @@ async function run() {
       const limitedService = await cursor.limit(3).toArray();
       res.send(limitedService);
 
-      //reviews start here
+      // reviews start here
+      app.get("/reviews", async (req, res) => {
+        let query = {};
+        if (req.query.email) {
+          query = {
+            email: req.query.email,
+          };
+        }
+        if (req.query.id) {
+          const sid = req.query.id;
+          query = { service : sid };
+        }
+        const cursor = reviewCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+
       app.post("/reviews", async (req, res) => {
         const review = req.body;
         const result = await reviewCollection.insertOne(review);
+        res.send(result);
+      });
+
+      app.delete("/reviews/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await reviewCollection.deleteOne(query);
         res.send(result);
       });
     });
